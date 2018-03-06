3180ee26c4de2f3d24bd7220dc86399b99a8a662
Sergio <sergio.paredes@est.fib.upc.edu>
2018-03-06 09:28:23 +0100

    Initial commit

---
+++ src/main/java/me/integrate/socialbank/SocialbankApplication.java
@@ -0,0 +1,12 @@
+package me.integrate.socialbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialbankApplication.class, args);
+	}
+}
