60cc2a4604a3118fb3f4c6b1f6e5a5a6ce784045
elena.alonso.gonzalez <elena.alonso.gonzalez@est.fib.upc.edu>
2018-03-26 01:17:06 +0200

    UserRepository

---
+++ src/main/java/me/integrate/socialbank/User/UserRepository.java
@@ -0,0 +1,16 @@
+package me.integrate.socialbank.User;
+
+import java.util.Date;
+
+public class UserRepository {
+    private final String USER_TABLE;
+    private final String EMAIL;
+    private final String NAME;
+    private final String SURNAME;
+    private final String PASSWORD;
+    private final String BIRTHDATE;
+    private final String GENDER;
+    private final String BALANCE;
+    private final String DESCRIPTION;
+
+}
