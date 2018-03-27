25728bf5c39951c32bf5afe7812c8767a73c576e
jbpextra <jbpextra@hotmail.com>
2018-03-27 13:20:33 +0200

    First commit

---
+++ src/main/java/me/integrate/socialbank/Event/Event.java
@@ -0,0 +1,72 @@
+package me.integrate.socialbank.Event;
+
+import me.integrate.socialbank.User.User;
+
+import java.util.Date;
+
+public class Event {
+
+    private int id;
+    private User creator;
+    private Date iniDate, endDate;
+    private int hours;
+    private String location;
+    private String title;
+    private String description;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User getCreator() {
+        return creator;
+    }
+
+    public void setCreator(User creator) {
+        this.creator = creator;
+    }
+
+    public Date getIniDate() {
+        return iniDate;
+    }
+
+    public void setIniDate(Date iniDate) {
+        this.iniDate = iniDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ src/main/resources/db/migration/V2__Create_event_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE "user" (
+  id VARCHAR(255) PRIMARY KEY,
+  creator VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  iniDate DATE NOT NULL,
+  endDate DATE NOT NULL,
+  hours INT NOT NULL,
+  location VARCHAR(255) NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  description TEXT
+);
