7465d68e98b638863f4817d15e21c4bcea8cc93b
jbpextra <jbpextra@hotmail.com>
2018-03-29 23:01:59 +0200

    Create Event service

---
+++ src/main/java/me/integrate/socialbank/event/EventService.java
@@ -0,0 +1,16 @@
+package me.integrate.socialbank.event;
+
+import me.integrate.socialbank.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class EventService
+{
+    EventRepository eventRepository;
+
+    @Autowired
+    public EventService(EventRepository eventRepository) { this.eventRepository = eventRepository; }
+
+    public Event getEventById(int id) { return eventRepository.getEventById(id); }
+
+    public Event saveEvent(Event event) { return eventRepository.saveEvent(event);}
+}
