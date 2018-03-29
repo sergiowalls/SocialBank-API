9ec914763486c78065144ff922798d5c23dd49c2
jbpextra <jbpextra@hotmail.com>
2018-03-29 22:18:09 +0200

    delete wrong attribute of Event table

--- src/main/resources/db/migration/V2__Create_event_table.sql
+++ src/main/resources/db/migration/V2__Create_event_table.sql
@@ -1,7 +1,6 @@
 CREATE TABLE "event" (
   id VARCHAR(255) PRIMARY KEY,
   creator VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL,
   iniDate DATE NOT NULL,
   endDate DATE NOT NULL,
   hours INT NOT NULL,
