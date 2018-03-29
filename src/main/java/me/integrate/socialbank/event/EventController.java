17aad14b2ea116f44a62515325f1d882c30e64f2
jbpextra <jbpextra@hotmail.com>
2018-03-29 23:07:38 +0200

    Create Event controller

--- src/main/java/me/integrate/socialbank/event/EventController.java
+++ src/main/java/me/integrate/socialbank/event/EventController.java
@@ -4,6 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-
+@RestController
 public class EventController {
+    EventService eventService;
+
+    @Autowired
+    public EventController(EventService eventService) { this.eventService = eventService; }
+
+    @GetMapping("/events/{id}")
+    public Event getEventByEmail(@PathVariable int id) { return eventService.getEventById(id); }
+
+    @PostMapping("/events")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Event saveEvent(@RequestBody Event event) { return eventService.saveEvent(event);}
+
 }
