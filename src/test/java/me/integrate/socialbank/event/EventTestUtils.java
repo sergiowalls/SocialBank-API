f28acdf7354bcb6858fa008edfa6d2ad815de1ff
jbpextra <jbpextra@hotmail.com>
2018-04-02 19:31:00 +0200

    Add more functions to EventTestUtils

--- src/test/java/me/integrate/socialbank/event/EventTestUtils.java
+++ src/test/java/me/integrate/socialbank/event/EventTestUtils.java
@@ -2,28 +2,33 @@ package me.integrate.socialbank.event;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 class EventTestUtils {
 
     static Event createEvent(int id) {
-        return createEvent(id, "pepito@pepito.com");
+        Date iniDate, endDate;
+        iniDate = endDate = new Date();
+        try {
+            iniDate = new SimpleDateFormat("yyyy-MM-dd").parse("2019-03-03");
+        } catch (ParseException ex) {
+            ex.printStackTrace();
+        }
+        try {
+           endDate = new SimpleDateFormat("yyyy-MM-dd").parse("2020-03-03");
+        } catch (ParseException ex) {
+            ex.printStackTrace();
+        }
+        return createEvent(id, iniDate, endDate);
     }
 
-    static Event createEvent(int id, String email) {
+    static Event createEvent(int id, Date iniDate, Date endDate) {
         Event event = new Event();
         event.setId(id);
-        event.setCreatorEmail(email);
+        event.setCreatorEmail("pepito@peito.com");
         event.setHours(13);
-        try {
-            event.setIniDate(new SimpleDateFormat("yyyy-MM-dd").parse("2019-03-03"));
-        } catch (ParseException ex) {
-            ex.printStackTrace();
-        }
-        try {
-            event.setEndDate(new SimpleDateFormat("yyyy-MM-dd").parse("2020-03-03"));
-        } catch (ParseException ex) {
-            ex.printStackTrace();
-        }
+        event.setIniDate(iniDate);
+        event.setEndDate(endDate);
         event.setLocation("53.2734, -7.778320310000026");
         event.setDescription("Worldwide Meeting");
         event.setTitle("Worldwide Meeting");
