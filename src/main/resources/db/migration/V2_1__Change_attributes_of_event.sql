8e79413052ee3952784267f9f85085d0855c6a55
jbpextra <jbpextra@hotmail.com>
2018-04-02 17:09:30 +0200

    Migrate DB 2.1 Change attributes of event

---
+++ src/main/resources/db/migration/V2_1__Change_attributes_of_event.sql
@@ -0,0 +1,12 @@
+DROP TABLE "event";
+
+CREATE TABLE "event" (
+  id INT PRIMARY KEY,
+  creatorEmail VARCHAR(255) NOT NULL,
+  iniDate DATE NOT NULL,
+  endDate DATE NOT NULL,
+  hours INT NOT NULL CHECK(hours > 0),
+  location VARCHAR(255) NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  description TEXT
+);
