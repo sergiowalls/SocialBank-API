0c99137ab7eaef9afdfaacf3f38c6c9dff4176b3
jbpextra <jbpextra@hotmail.com>
2018-04-02 12:51:16 +0200

    Add EventTestUtils

---
+++ src/test/java/me/integrate/socialbank/event/EventTestUtils.java
@@ -0,0 +1,32 @@
+package me.integrate.socialbank.event;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class EventTestUtils {
+
+    static Event createEvent(int id) {
+        return createEvent(id, "pepito@pepito.com");
+    }
+
+    static Event createEvent(int id, String email) {
+        Event event = new Event();
+        event.setId(id);
+        event.setCreatorEmail(email);
+        event.setHours(13);
+        try {
+            event.setIniDate(new SimpleDateFormat("yyyy-MM-dd").parse("2019-03-03"));
+        } catch (ParseException ex) {
+            ex.printStackTrace();
+        }
+        try {
+            event.setEndDate(new SimpleDateFormat("yyyy-MM-dd").parse("2020-03-03"));
+        } catch (ParseException ex) {
+            ex.printStackTrace();
+        }
+        event.setLocation("53.2734, -7.778320310000026");
+        event.setDescription("Worldwide Meeting");
+        event.setTitle("Worldwide Meeting");
+        return event;
+    }
+}
