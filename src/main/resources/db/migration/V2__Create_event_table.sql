72aaa9b1b0c24ee430c8f8c80c56be4c6b016dcc
jbpextra <jbpextra@hotmail.com>
2018-04-02 16:51:54 +0200

    change DB Event table attribute types

--- src/main/resources/db/migration/V2__Create_event_table.sql
+++ src/main/resources/db/migration/V2__Create_event_table.sql
@@ -1,9 +1,9 @@
 CREATE TABLE "event" (
-  id VARCHAR(255) PRIMARY KEY,
+  id INT PRIMARY KEY,
   creatorEmail VARCHAR(255) NOT NULL,
   iniDate DATE NOT NULL,
   endDate DATE NOT NULL,
-  hours INT NOT NULL,
+  hours INT NOT NULL CHECK(hours > 0),
   location VARCHAR(255) NOT NULL,
   title VARCHAR(255) NOT NULL,
   description TEXT
