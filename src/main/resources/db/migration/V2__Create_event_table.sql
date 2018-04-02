c8a60a83c428e66bc3f875abb0d5efcc513e448c
jbpextra <jbpextra@hotmail.com>
2018-04-02 17:07:04 +0200

    Revert "change DB Event table attribute types"
    This reverts commit 72aaa9b

--- src/main/resources/db/migration/V2__Create_event_table.sql
+++ src/main/resources/db/migration/V2__Create_event_table.sql
@@ -1,9 +1,9 @@
 CREATE TABLE "event" (
-  id INT PRIMARY KEY,
+  id VARCHAR(255) PRIMARY KEY,
   creatorEmail VARCHAR(255) NOT NULL,
   iniDate DATE NOT NULL,
   endDate DATE NOT NULL,
-  hours INT NOT NULL CHECK(hours > 0),
+  hours INT NOT NULL,
   location VARCHAR(255) NOT NULL,
   title VARCHAR(255) NOT NULL,
   description TEXT
