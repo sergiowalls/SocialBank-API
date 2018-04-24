a0a17054cfcdaedb497093a97564c3b57fda9a71
Sergio <sergio.paredes@est.fib.upc.edu>
2018-04-24 12:34:40 +0200

    Disable swagger validation

--- src/main/java/me/integrate/socialbank/SocialBankApplication.java
+++ src/main/java/me/integrate/socialbank/SocialBankApplication.java
@@ -5,10 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger.web.UiConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -27,9 +31,26 @@ public class SocialBankApplication {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("SocialBank API Documentation")
+                .description("API for managing endpoints")
+                .version("1.0")
+                .build();
+    }
+
+    @Bean
+    public UiConfiguration uiConfig() {
+        return UiConfigurationBuilder.builder()
+                .displayRequestDuration(true)
+                .validatorUrl("") // Disable the validator to avoid "Error" at the bottom of the Swagger UI page
                 .build();
     }
 }
