1e4e6943491548db035c68425c60284eed6d8e35
Sergio <sergio.paredes@est.fib.upc.edu>
2018-04-24 12:47:29 +0200

    Extract swagger configuration

--- src/main/java/me/integrate/socialbank/SocialBankApplication.java
+++ src/main/java/me/integrate/socialbank/SocialBankApplication.java
@@ -4,19 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.RestController;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger.web.UiConfiguration;
-import springfox.documentation.swagger.web.UiConfigurationBuilder;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class SocialBankApplication {
 
     public static void main(String[] args) {
@@ -28,29 +17,4 @@ public class SocialBankApplication {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
-                .paths(PathSelectors.any())
-                .build();
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("SocialBank API Documentation")
-                .description("API for managing endpoints")
-                .version("1.0")
-                .build();
-    }
-
-    @Bean
-    public UiConfiguration uiConfig() {
-        return UiConfigurationBuilder.builder()
-                .displayRequestDuration(true)
-                .validatorUrl("") // Disable the validator to avoid "Error" at the bottom of the Swagger UI page
-                .build();
-    }
 }
