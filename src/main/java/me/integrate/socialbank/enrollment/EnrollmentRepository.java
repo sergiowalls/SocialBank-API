5bd2a912df8d8ae25c5a3d106d975756d013d86b
jbpextra <jbpextra@hotmail.com>
2018-05-19 15:42:49 +0200

    Revert "Implement User-Event Enrollments"
    This reverts commit 2f559b6

--- src/main/java/me/integrate/socialbank/enrollment/EnrollmentRepository.java
+++
@@ -1,6 +0,0 @@
-package me.integrate.socialbank.enrollment;
-
-public interface EnrollmentRepository {
-
-    Enrollment enrollUserEvent(Enrollment enrollment);
-}
--- src/main/java/me/integrate/socialbank/enrollment/exceptions/EnrollmentAlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-package me.integrate.socialbank.enrollment.exceptions;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.CONFLICT, reason = "Already enrolled")
-public class EnrollmentAlreadyExistsException extends RuntimeException {
-}
