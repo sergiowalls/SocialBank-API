b5d4a9abf78396f38b6d316786b0404351ff5740
jbpextra <jbpextra@hotmail.com>
2018-05-19 17:43:32 +0200

    Add more Enrollment Repository tests

--- src/test/java/me/integrate/socialbank/enrollment/EnrollmentRepositoryTest.java
+++ src/test/java/me/integrate/socialbank/enrollment/EnrollmentRepositoryTest.java
@@ -10,7 +10,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 @Transactional
@@ -38,4 +42,45 @@ public class EnrollmentRepositoryTest {
         Enrollment enrollment = new Enrollment(email, id);
         assertEquals(enrollment, enrollmentRepository.saveEnrollment(enrollment));
     }
+
+    @Test
+    void givenEnrollmentsStoredInDatabaseWhenRetrievedByEventReturnEnrollments() {
+        String emailCreator = "a@a.a", emailEnrolledOne = "b@b.b", emailEnrolledTwo = "c@c.c";
+
+        userRepository.saveUser(UserTestUtils.createUser(emailCreator));
+        userRepository.saveUser(UserTestUtils.createUser(emailEnrolledOne));
+        userRepository.saveUser(UserTestUtils.createUser(emailEnrolledTwo));
+
+        int id = eventRepository.saveEvent(EventTestUtils.createEvent(emailCreator)).getId();
+        List<String> le = new ArrayList<>();
+        le.add(emailEnrolledOne); le.add(emailEnrolledTwo);
+
+        enrollmentRepository.saveEnrollment(new Enrollment(emailEnrolledOne, id));
+        enrollmentRepository.saveEnrollment(new Enrollment(emailEnrolledTwo, id));
+
+        List<String> retList = enrollmentRepository.getEnrollmentsOfEvent(id);
+        assertTrue(le.containsAll(retList));
+        assertTrue(retList.containsAll(le));
+    }
+
+    @Test
+    void givenEnrollmentsStoredInDatabaseWhenRetrievedByUserReturnEnrollments() {
+        String emailCreator = "a@a.a", emailEnrolled = "b@b.b";
+
+        userRepository.saveUser(UserTestUtils.createUser(emailCreator));
+        userRepository.saveUser(UserTestUtils.createUser(emailEnrolled));
+
+        int idOne = eventRepository.saveEvent(EventTestUtils.createEvent(emailCreator)).getId();
+        int idTwo = eventRepository.saveEvent(EventTestUtils.createEvent(emailCreator)).getId();
+        List<Integer> le = new ArrayList<>();
+        le.add(idOne); le.add(idTwo);
+
+        enrollmentRepository.saveEnrollment(new Enrollment(emailEnrolled, idOne));
+        enrollmentRepository.saveEnrollment(new Enrollment(emailEnrolled, idTwo));
+
+        List<Integer> retList = enrollmentRepository.getEnrollmentsOfUser(emailEnrolled);
+        assertTrue(le.containsAll(retList));
+        assertTrue(retList.containsAll(le));
+    }
+
 }
