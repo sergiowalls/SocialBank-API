551f5a87032fa5185fe2acdb6e63d29fed71e682
jbpextra <jbpextra@hotmail.com>
2018-05-20 16:43:12 +0200

    Add Post repo Interface
    Signed-off-by: jbpextra <jbpextra@hotmail.com>

---
+++ src/main/java/me/integrate/socialbank/post/PostRepository.java
@@ -0,0 +1,15 @@
+package me.integrate.socialbank.post;
+
+import java.util.List;
+
+public interface PostRepository {
+    Post getPostById(int event_id, int id);
+
+    Post updateContent(int event_id, int id, String content);
+
+    Post savePost(Post post);
+
+    List<Post> getAllPosts(int event_id);
+
+    void deletePost(int event_id, int id);
+}
