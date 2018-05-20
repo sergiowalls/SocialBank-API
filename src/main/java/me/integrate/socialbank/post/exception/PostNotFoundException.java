0454003ab5f885c64afeeeb6621d23f2c470c18f
jbpextra <jbpextra@hotmail.com>
2018-05-20 19:11:04 +0200

    Add exception Http response
    Signed-off-by: jbpextra <jbpextra@hotmail.com>

--- src/main/java/me/integrate/socialbank/post/exception/PostNotFoundException.java
+++ src/main/java/me/integrate/socialbank/post/exception/PostNotFoundException.java
@@ -1,4 +1,8 @@
 package me.integrate.socialbank.post.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Post not found")
 public class PostNotFoundException extends RuntimeException {
 }
