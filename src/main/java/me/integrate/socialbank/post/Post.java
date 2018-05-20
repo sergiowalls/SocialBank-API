167cf25e57559b4a11b045903f35bd7fda7e6076
jbpextra <jbpextra@hotmail.com>
2018-05-20 16:26:55 +0200

    refactor Post model
    Signed-off-by: jbpextra <jbpextra@hotmail.com>

--- src/main/java/me/integrate/socialbank/post/Post.java
+++ src/main/java/me/integrate/socialbank/post/Post.java
@@ -1,47 +1,65 @@
 package me.integrate.socialbank.post;
 
-import org.springframework.lang.Nullable;
-
 import java.util.Date;
 
 public class Post {
     private int id;
-    private Date created_at;
-    private Date updated_at;
-    @Nullable
-    private int answer_to;
+    private String creatorEmail;
+    private Date createdAt;
+    private Date updatedAt;
+    private int answerTo;
     private String content;
 
-    public Post(int id, int answer_to, String content) {
-        this.id = id;
-        this.answer_to = answer_to;
-        this.content = content;
-        created_at = new Date();
-        updated_at = new Date();
+    public Post() {
+        createdAt = new Date();
+        updatedAt = new Date();
     }
 
-    public String getContent() {
-        return content;
+
+    private void update() {
+        this.updatedAt = new Date();
     }
 
-    public void setContent(String content) {
-        updated_at = new Date();
-        this.content = content;
-    }
 
     public int getId() {
         return id;
     }
 
-    public Date getCreated_at() {
-        return created_at;
+    public Date getCreatedAt() {
+        return createdAt;
     }
 
-    public Date getUpdated_at() {
-        return updated_at;
+    public Date getUpdatedAt() {
+        return updatedAt;
     }
 
-    public int getAnswer_to() {
-        return answer_to;
+    public int getAnswerTo() {
+        return answerTo;
+    }
+
+    public String getCreatorEmail() {
+        return creatorEmail;
+    }
+
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCreatorEmail(String creatorEmail) {
+        this.creatorEmail = creatorEmail;
+    }
+
+    public void setAnswerTo(int answerTo) {
+        this.answerTo = answerTo;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+        update();
     }
 }
