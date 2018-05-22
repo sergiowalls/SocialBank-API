3e7e6fd95c2a77a332eca8db9162286f729729d3
jbpextra <jbpextra@hotmail.com>
2018-05-22 13:39:07 +0200

    clean code

--- src/main/java/me/integrate/socialbank/enrollment/Enrollment.java
+++ src/main/java/me/integrate/socialbank/enrollment/Enrollment.java
@@ -6,26 +6,9 @@ public class Enrollment {
     private String userEmail;
     private int eventId;
 
-    public Enrollment() {}
 
     public Enrollment(String userEmail, int eventId) {
         this.userEmail = userEmail;
-        this.eventId = eventId;
-    }
-
-    public String getUserEmail() {
-        return userEmail;
-    }
-
-    public void setUserEmail(String userEmail) {
-        this.userEmail = userEmail;
-    }
-
-    public int getEventId() {
-        return eventId;
-    }
-
-    public void setEventId(int eventId) {
         this.eventId = eventId;
     }
 
