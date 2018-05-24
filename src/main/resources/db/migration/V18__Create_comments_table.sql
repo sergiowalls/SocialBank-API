5727eb1d11714b322b28e87eb8c79092a5622997
jbpextra <jbpextra@hotmail.com>
2018-05-24 23:41:28 +0200

    fix sql

--- src/main/resources/db/migration/V18__Create_comments_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE comments (
-  id            serial PRIMARY KEY,
-  creator_email varchar(255) NOT NULL,
-  created_at    timestamp    NOT NULL,
-  updated_at    timestamp    NOT NULL,
-  answer_to     integer REFERENCES comments (id) ON DELETE CASCADE,
-  content       text         NOT NULL
-);
