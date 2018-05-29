9a09c5f387045ccb718f2dbfea2397db019348fe
jbpextra <jbpextra@hotmail.com>
2018-05-29 13:02:09 +0200

    Check user hours balance

--- src/main/java/me/integrate/socialbank/enrollment/EnrollmentService.java
+++ src/main/java/me/integrate/socialbank/enrollment/EnrollmentService.java
@@ -1,11 +1,10 @@
 package me.integrate.socialbank.enrollment;
 
-import me.integrate.socialbank.enrollment.exceptions.EventIsClosedException;
-import me.integrate.socialbank.enrollment.exceptions.TooLateException;
-import me.integrate.socialbank.enrollment.exceptions.UserIsNotEnrolledException;
-import me.integrate.socialbank.enrollment.exceptions.UserIsTheCreatorException;
+import me.integrate.socialbank.enrollment.exceptions.*;
 import me.integrate.socialbank.event.Event;
 import me.integrate.socialbank.event.EventService;
+import me.integrate.socialbank.user.User;
+import me.integrate.socialbank.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
@@ -18,23 +17,33 @@ public class EnrollmentService {
 
     private EnrollmentRepository enrollmentRepository;
     private EventService eventService;
+    private UserService userService;
+
+    private float getTimDiff(Date iniDate, Date endDate) {
+        return (iniDate.getTime() - endDate.getTime()) / (60 * 60 * 1000); //convert from millisecond to hour
+    }
 
     @Autowired
-    public EnrollmentService(EnrollmentRepository enrollmentRepository, EventService eventService) {
+    public EnrollmentService(EnrollmentRepository enrollmentRepository, EventService eventService, UserService
+            userService) {
         this.enrollmentRepository = enrollmentRepository;
         this.eventService = eventService;
+        this.userService = userService;
     }
 
     public Enrollment saveEnrollment(int id, String email) {
         Event event = eventService.getEventById(id);
+        String creatorEmail = event.getCreatorEmail();
+        User user = userService.getUserByEmail(creatorEmail);
         Date eventDate = event.getIniDate();
         if (eventDate != null) {
             if (eventDate.before(new Date())) throw new TooLateException();
         } else {
             if (event.isClosed()) throw new EventIsClosedException();
         }
-
-        if (event.getCreatorEmail().equals(email)) throw new UserIsTheCreatorException();
+        if (creatorEmail.equals(email)) throw new UserIsTheCreatorException();
+        if (user.getBalance() < getTimDiff(event.getIniDate(), event.getEndDate()))
+            throw new UserDoesNotHaveEnoughHours();
         return enrollmentRepository.saveEnrollment(id, email);
     }
 
