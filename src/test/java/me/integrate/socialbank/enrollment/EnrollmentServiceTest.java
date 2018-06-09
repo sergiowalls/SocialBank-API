c29dbc33971eef936a87b0a9f40d1cebd18a5b72
jbpextra <jbpextra@hotmail.com>
2018-06-09 08:27:04 +0200

    add basic number of enrollments test

--- src/test/java/me/integrate/socialbank/enrollment/EnrollmentServiceTest.java
+++ src/test/java/me/integrate/socialbank/enrollment/EnrollmentServiceTest.java
@@ -105,6 +105,21 @@ public class EnrollmentServiceTest {
     }
 
     @Test
+    void whenAddOrDeleteEnrollmentThenEventIsCorrectlyUpdated() {
+        String creatorEmail = "email@email.tld";
+        String enrolledEmail = "e@e.e";
+        userService.saveUser(UserTestUtils.createUser(creatorEmail));
+        userService.saveUser(UserTestUtils.createUser(enrolledEmail));
+        Event event = eventService.saveEvent(EventTestUtils.createEvent(creatorEmail));
+        assert (event.getNumberEnrolled() == 0);
+        int eventId = event.getId();
+        enrollmentService.saveEnrollment(eventId, enrolledEmail);
+        assert (event.getNumberEnrolled() == 1);
+        enrollmentService.deleteEnrollment(eventId, enrolledEmail);
+        assert (event.getNumberEnrolled() == 0);
+    }
+
+    @Test
     void whenEnrollUnder24HourBeforeIniDateThenShouldReturnTooLateException() {
         String emailCreator = ("a@a.com");
         String emailEnrolled = ("z@z.z");
