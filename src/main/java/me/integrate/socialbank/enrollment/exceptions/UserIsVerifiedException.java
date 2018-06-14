732c3a1ed195991761c1f6be0e5b72db7865ccce
jbpextra <jbpextra@hotmail.com>
2018-06-14 20:13:16 +0200

    move UserIsVerifiedException file
    Signed-off-by: jbpextra <jbpextra@hotmail.com>

--- src/main/java/me/integrate/socialbank/enrollment/exceptions/UserIsVerifiedException.java
+++ src/main/java/me/integrate/socialbank/enrollment/exceptions/UserIsVerifiedException.java
@@ -1,4 +1,4 @@
-package me.integrate.socialbank.enrollment;
+package me.integrate.socialbank.enrollment.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
